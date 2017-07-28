5aa9961439f3dd03733c13c6005e0bc6c7d3767a
Patricio López <lopezjuripatricio@gmail.com>
2017-07-28 17:20:08 -0400

    Refactor CLI

--- packages/chat-intent-cli/bin/chat-intent.js
+++ packages/chat-intent-cli/bin/chat-intent.js
@@ -5,4 +5,6 @@
 const yargs = require("yargs");
 const createCLI = require("..").default;
 
-createCLI(yargs, console.log.bind(console));
+const print = console.log.bind(console);
+
+createCLI(yargs, print).demandCommand().help().wrap(72).argv;
--- packages/chat-intent-cli/src/index.js
+++ packages/chat-intent-cli/src/index.js
@@ -10,7 +10,7 @@ import startsWith from "lodash/startsWith";
 export default function createCLI(builder, print) {
   builder = builder.command({
     command: "generate <phone> [country]",
-    aliases: ["gen", "*"],
+    aliases: ["gen", "g"],
     desc: "Get URIs to start a conversation.",
     builder: yargs => {
       yargs.option("services", {
@@ -54,11 +54,9 @@ export default function createCLI(builder, print) {
       if (!result.valid) {
         return print("Invalid number.");
       }
-      print(prettyjson.render(result));
+      return print(prettyjson.render(result));
     },
   });
-
-  builder = builder.demandCommand().help().wrap(72).argv;
 
   return builder;
 }
